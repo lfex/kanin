019c016ebf2368d9f79dced1386d10e44e25a05e
Duncan McGreggor <oubiwann@gmail.com>
2017-02-04 01:15:27 -0600

    Use master (it has the app.src file in it).

--- resources/make/deps.mk
+++ resources/make/deps.mk
@@ -1,9 +1,9 @@
 SILENT_MAKE = $(MAKE) --no-print-directory --silent
 DEPS_DIR = $(shell pwd)/_build/default/lib
 AMQP_DIR = $(DEPS_DIR)/amqp_client
-AMQP_REPO = "https://github.com/rabbitmq/rabbitmq-erlang-client.git"
+AMQP_REPO = "https://github.com/lfe-support/rabbitmq-erlang-client.git"
 RABCOM_DIR = $(DEPS_DIR)/rabbit_common
-RAB_VER = "rabbitmq_v3_7_0_milestone10"
+RAB_VER = "master"
 
 amqp-client:
 	@cd $(AMQP_DIR) && \
